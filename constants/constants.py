81e3e52d918d4c9c32390282a4b0f25d3056861a
astroJeff <andrews@astro.columbia.edu>
2016-04-16 19:19:09 +0300

    Created  constants/constants.py

---
+++ constants/constants.py
@@ -0,0 +1,28 @@
+G = 6.674e-8 # Gravitational constant in cgs
+GGG = 1.909e5 # Gravitational constant in Rsun * (km/s)^2 / Msun
+km_to_cm = 1.0e5 # km to cm
+Msun_to_g = 1.989e33 # Msun to g
+Rsun_to_cm = 6.995e10 # Rsun to cm
+AU_to_cm = 1.496e13 # AU to cm
+pc_to_cm = 3.086e18 # parsec to cm
+pc_to_km = 3.086e13 # parsec to km
+yr_to_sec = 31557600.0 # Sec in yr
+day_to_sec = 3600.0*24.0 # Sec in day
+asec_to_rad = 4.84814e-6 # Arcsec to radians
+
+M_0 = 0.073 # in Msun
+xi = 0.704
+M_NS = 1.35
+#v_wind = 1000.0 # companion velocity in km/s
+R_NS = 12.0  # NS radius in km
+
+
+min_mass = 8.0
+max_mass = 39.0
+min_A = 1.0e2
+max_A = 1.0e4
+v_k_sigma = 265.0
+alpha = -2.35
+
+dist_LMC = 5.0e4 * pc_to_km # Distance in km
+dist_SMC = 6.1e4 * pc_to_km # Distance in km
