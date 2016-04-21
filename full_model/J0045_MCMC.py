dbbe112d8cdda3e419b2d6e08becd45cc1e4a618
astroJeff <andrews@astro.columbia.edu>
2016-04-21 17:58:13 +0300

    Added output time to _MCMC.py program

--- full_model/J0045_MCMC.py
+++ full_model/J0045_MCMC.py
@@ -4,13 +4,14 @@ import sys
 from astropy.coordinates import SkyCoord
 from astropy import units as u
 import pickle
+import time
 
 sys.path.append('../stats')
 import stats
 
 
 
-
+# Set J0045 parameters
 coor_J0045 = SkyCoord('00h45m35.26s', '-73d19m03.32s')
 
 ra_J0045 = coor_J0045.ra.degree
@@ -19,6 +20,11 @@ M2_d_J0045 = 11.0  # M2 in Msun
 P_orb_J0045 = 51.17  # P_orb in days
 ecc_J0045 = 0.808  # eccentricity
 
-sampler = stats.run_emcee(M2_d_J0045, P_orb_J0045, ecc_J0045, ra_J0045, dec_J0045, nburn=10000, nsteps=500000)
+# Record time
+start_time = time.time()
+
+sampler = stats.run_emcee(M2_d_J0045, P_orb_J0045, ecc_J0045, ra_J0045, dec_J0045, nburn=10000, nsteps=10000)
+
+print "Simulation took", time.time()-start_time, "seconds"
 
 pickle.dump( sampler, open( "../data/J0045_MCMC_sampler.obj", "wb" ) )
