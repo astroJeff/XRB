8b461f0cc33e80c8d2d4a367337e46bd6a87d0f3
AstroJeff <andrews@astro.columbia.edu>
2016-05-02 00:44:34 +0300

    Added v_k vs. theta plot to smc_population_plots.py

--- full_model/smc_population_plots.py
+++ full_model/smc_population_plots.py
@@ -35,18 +35,18 @@ plt.savefig('../figures/smc_population_chains.pdf')
 
 # Corner plot
 labels = [r"$M_1$", r"$M_2$", r"$A$", r"$e$", r"$v_k$", r"$\theta$", r"$\phi$", r"$\alpha_{\rm b}$", r"$\delta_{\rm b}$", r"$t_{\rm b}$"]
-truths = [M1_true, M2_true, A_true, ecc_true, v_k_true, theta_true, phi_true, ra_true, dec_true, t_b_true]
-fig = corner.corner(sampler.flatchain, labels=labels, truths=truths)
+fig = corner.corner(sampler.flatchain, labels=labels)
 plt.rc('font', size=18)
 plt.savefig('../figures/smc_population_corner.pdf')
 plt.rc('font', size=10)
 
 
 
+
 # M1 vs. M2
-plt.subplot(4,1,1)
+#plt.subplot(4,1,1)
 corner.hist2d(sampler.flatchain.T[0], sampler.flatchain.T[1])
-plt.scatter(init_params["M1"], init_params["M2"], color='r')
+#plt.scatter(init_params["M1"], init_params["M2"], color='r')
 plt.xlabel(r"$M_1$", size=16)
 plt.ylabel(r"$M_2$", size=16)
 #plt.xlim(8.5, 12.0)
@@ -54,11 +54,22 @@ plt.ylabel(r"$M_2$", size=16)
 plt.savefig('../figures/smc_population_M1_M2.pdf')
 
 # Orbital separation vs. eccentricity
-plt.subplot(4,1,2)
+#plt.subplot(4,1,2)
 corner.hist2d(sampler.flatchain.T[2], sampler.flatchain.T[3])
-plt.scatter(init_params["A"], init_params["ecc"], color='r')
+#plt.scatter(init_params["A"], init_params["ecc"], color='r')
 plt.xlabel(r"$a$", size=16)
 plt.ylabel(r"$e$", size=16)
 #plt.xlim(10.0, 1500.0)
 #plt.ylim(0.0, 1.0)
 plt.savefig('../figures/smc_population_A_ecc.pdf')
+
+# v_kick vs. theta
+corner.hist2d(sampler.flatchain.T[4], sampler.flatchain.T[5])
+plt.xlabel(r"$v_k$")
+plt.ylabel(r"$\theta$")
+plt.savefig("../figures/smc_population_vk_theta.pdf")
+
+
+
+
+
