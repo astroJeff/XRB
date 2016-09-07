b44fe564ba3123f312e43103fda31d8aa4f8b1ee
AstroJeff <andrews@astro.columbia.edu>
2016-09-07 11:45:15 +0300

    Updated smc_population scripts

--- full_model/smc_population.py
+++ full_model/smc_population.py
@@ -16,4 +16,10 @@ sampler = stats.run_emcee_population(nburn=10000, nsteps=50000)
 
 print "Simulation took", time.time()-start_time, "seconds"
 
+
+print "Production run:"
+print "Autocorrelation lengths", sampler.acor
+print "Acceptance fraction", sampler.acceptance_fraction
+
+
 pickle.dump( sampler, open( "../data/SMC_MCMC_sampler.obj", "wb" ) )
--- full_model/smc_population_plots.py
+++ full_model/smc_population_plots.py
@@ -23,9 +23,14 @@ sys.path.append('../constants')
 import constants as c
 
 
+print "Loading data"
+
 # Load sampler using pickle
 sampler = pickle.load( open( "../data/SMC_MCMC_sampler.obj", "rb" ) )
 HMXB = pickle.load( open("../data/SMC_MCMC_HMXB.obj", "rb" ) )
+
+
+print "Finished loading data"
 
 # # Chains plot
 # fig, ax = plt.subplots(sampler.dim, 1, sharex=True, figsize=(7.0,20.0))
@@ -36,35 +41,17 @@ HMXB = pickle.load( open("../data/SMC_MCMC_HMXB.obj", "rb" ) )
 #         ax[i].yaxis.set_major_locator(yloc)
 # fig.subplots_adjust(hspace=0)
 # plt.yticks(fontsize = 8)
-# plt.savefig('../figures/smc_population_chains.pdf', rasterized=True)
+#plt.savefig('../figures/smc_population_chains.pdf', rasterized=True)
 
 
-# # Corner plot
-# labels = [r"$M_1$", r"$M_2$", r"$a$", r"$e$", r"$v_k$", r"$\theta$", r"$\phi$", r"$\alpha_{\rm b}$", r"$\delta_{\rm b}$", r"$t_{\rm b}$"]
+# Corner plot
+# labels = [r"$M_1\ (M_{\odot})$", r"$M_2\ (M_{\odot})$", r"$a\ (R_{\odot})$", r"$e$", r"$v_k\ ({\rm km}\ {\rm s}^{-1})$", r"$\theta\ ({\rm rad})$", r"$\phi\ ({\rm rad})$", r"$\alpha_{\rm b}\ ({\rm deg})$", r"$\delta_{\rm b}\ ({\rm deg}) $", r"$t_{\rm b}\ ({\rm Myr})$"]
 # plt_range = ([7,24], [2.5,15], [0,1500], [0,1], [0,450], [0,np.pi], [0,2.0*np.pi], [6,21], [-76,-70], [0,70])
 # hist2d_kwargs = {"plot_datapoints" : False}
 # fig = corner.corner(sampler.flatchain, labels=labels, range=plt_range, bins=40, max_n_ticks=4, **hist2d_kwargs)
 # plt.rc('font', size=18)
 # plt.savefig('../figures/smc_population_corner.pdf')
 # plt.rc('font', size=10)
-
-
-# # M1 vs. M2
-# fig, host = plt.subplots(figsize=(5,5))
-# plt_range = ([7,25], [2.5,15])
-# corner.hist2d(sampler.flatchain.T[0], sampler.flatchain.T[1], bins=40, range=plt_range, plot_datapoints=False)
-# plt.xlabel(r"$M_1$", size=16)
-# plt.ylabel(r"$M_2$", size=16)
-# plt.savefig('../figures/smc_population_M1_M2.pdf')
-
-# # Orbital separation vs. eccentricity
-# fig, host = plt.subplots(figsize=(5,5))
-# plt_range = ([0,2000], [0,1])
-# corner.hist2d(sampler.flatchain.T[2], sampler.flatchain.T[3], bins=40, range=plt_range, plot_datapoints=False)
-# plt.xlabel(r"$a$", size=16)
-# plt.ylabel(r"$e$", size=16)
-# plt.savefig('../figures/smc_population_A_ecc.pdf')
-
 
 
 
@@ -95,8 +82,8 @@ HMXB = pickle.load( open("../data/SMC_MCMC_HMXB.obj", "rb" ) )
 #     ran_phi = pop_synth.get_phi(1)
 #
 #     # Get the new ra and dec
-#     ra_out, dec_out = pop_synth.get_new_ra_dec(s[7], s[8], data_out[7], ran_phi)
-#
+#    ra_out, dec_out = pop_synth.get_new_ra_dec(s[7], s[8], data_out[7], ran_phi)
+# 
 #     # Get the output orbital period
 #     Porb = binary_evolve.A_to_P(data_out[0], data_out[1], data_out[5])
 # 
@@ -109,7 +96,7 @@ HMXB = pickle.load( open("../data/SMC_MCMC_HMXB.obj", "rb" ) )
 #     HMXB_vsys[i] = data_out[3]
 #     HMXB_Lx[i] = data_out[2]
 #     HMXB_theta[i] = data_out[7] * 180.0*60.0/np.pi
-#
+# 
 # # Save current HMXB parameters as an object
 # HMXB = np.array([HMXB_ra, HMXB_dec, HMXB_Porb, HMXB_ecc, HMXB_M2, HMXB_vsys, HMXB_Lx, HMXB_theta])
 # pickle.dump( HMXB, open( "../data/SMC_MCMC_HMXB.obj", "wb" ) )
@@ -119,43 +106,44 @@ HMXB = pickle.load( open("../data/SMC_MCMC_HMXB.obj", "rb" ) )
 
 
 
-# HMXB Orbital period vs. eccentricity
-fig, ax = plt.subplots(3, 1, figsize=(6,8))
-plt.rc('font', size=14)
-plt_range = ([0, 4], [0,1])
-corner.hist2d(np.log10(HMXB[2]), HMXB[3], ax=ax[0], bins=40, range=plt_range, plot_datapoints=False)
-ax[0].set_xlabel(r"${\rm log}\ P_{\rm orb}\ {\rm (days)}$", size=16)
-ax[0].set_ylabel(r"$e$", size=16)
-ax[0].set_xticks([0,1,2,3,4])
-ax[0].set_yticks([0,0.25,0.5,0.75,1.0])
 
-plt_range = ([8, 24], [0,80])
-corner.hist2d(HMXB[4], HMXB[5], ax=ax[1], bins=40, range=plt_range, plot_datapoints=False)
-ax[1].set_xlabel(r"$M_2\ ({\rm M}_{\odot})$", size=16)
-ax[1].set_ylabel(r"$v_{\rm sys}\ {\rm (km\ s}^{-1})$", size=16)
-ax[1].set_xticks([8,12,16,20,24])
-ax[1].set_yticks([0,20, 40, 60, 80])
+# # HMXB Orbital period vs. eccentricity
+# fig, ax = plt.subplots(3, 1, figsize=(6,8))
+# plt.rc('font', size=14)
+# plt_range = ([0, 4], [0,1])
+# corner.hist2d(np.log10(HMXB[2]), HMXB[3], ax=ax[0], bins=40, range=plt_range, plot_datapoints=False)
+# ax[0].set_xlabel(r"${\rm log}\ P_{\rm orb}\ {\rm (days)}$", size=16)
+# ax[0].set_ylabel(r"$e$", size=16)
+# ax[0].set_xticks([0,1,2,3,4])
+# ax[0].set_yticks([0,0.25,0.5,0.75,1.0])
+# 
+# plt_range = ([8, 24], [0,80])
+# corner.hist2d(HMXB[4], HMXB[5], ax=ax[1], bins=40, range=plt_range, plot_datapoints=False)
+# ax[1].set_xlabel(r"$M_2\ ({\rm M}_{\odot})$", size=16)
+# ax[1].set_ylabel(r"$v_{\rm sys}\ {\rm (km\ s}^{-1})$", size=16)
+# ax[1].set_xticks([8,12,16,20,24])
+# ax[1].set_yticks([0,20, 40, 60, 80])
+# 
+# 
+# # Get flight time from birth time and M1 lifetime
+# load_sse.load_sse()
+# t_flight = sampler.flatchain.T[9] - load_sse.func_sse_tmax(sampler.flatchain.T[0])
+# plt_range = ([0,55], [0,25])
+# contour_kwargs = {'colors':'r', 'linestyles':'dashed'}
+# corner.hist2d(t_flight, HMXB[7], ax=ax[2], bins=40, range=plt_range, plot_density=False, 
+# 		plot_datapoints=False, contour_kwargs=contour_kwargs)
+# corner.hist2d(sampler.flatchain.T[9], HMXB[7], ax=ax[2], bins=40, range=plt_range, 
+# 		plot_density=False, plot_datapoints=False)
+# ax[2].set_xlabel(r"$t\ {\rm (Myr)}$", size=16)
+# ax[2].set_ylabel(r"$\theta\ {\rm (amin)}$", size=16)
+# 
+# plt.tight_layout()
+# 
+# plt.savefig('../figures/smc_population_HMXB.pdf')
 
 
-# Get flight time from birth time and M1 lifetime
-load_sse.load_sse()
-t_flight = sampler.flatchain.T[9] - load_sse.func_sse_tmax(sampler.flatchain.T[0])
-plt_range = ([0,55], [0,25])
-contour_kwargs = {'colors':'r'}
-corner.hist2d(t_flight, HMXB[7], ax=ax[2], bins=40, range=plt_range, plot_density=False, 
-		plot_datapoints=False, contour_kwargs=contour_kwargs)
-corner.hist2d(sampler.flatchain.T[9], HMXB[7], ax=ax[2], bins=40, range=plt_range, 
-		plot_density=False, plot_datapoints=False)
-ax[2].set_xlabel(r"$t\ {\rm (Myr)}$", size=16)
-ax[2].set_ylabel(r"$\theta\ {\rm (amin)}$", size=16)
 
-plt.tight_layout()
-
-plt.savefig('../figures/smc_population_HMXB.pdf')
-
-
-
-
+plt.rc('font', size=10)
 
 # Birth location
 # fig, host = plt.subplots(figsize=(5,5))
@@ -163,10 +151,12 @@ plt.savefig('../figures/smc_population_HMXB.pdf')
 # plt_kwargs = {'colors':'k'}
 # density_contour.density_contour(sampler.flatchain.T[7], sampler.flatchain.T[8], nbins_x=40, nbins_y=40, **plt_kwargs)
 # plt.tight_layout()
-plt.figure(figsize=(8,6))
+plt.figure(figsize=(4,4))
 sf_history.get_SMC_plot_polar(40.0, ra_dist=sampler.flatchain.T[7], dec_dist=sampler.flatchain.T[8], xwidth=3.0, ywidth=3.0)
 plt.tight_layout()
 plt.savefig('../figures/smc_population_ra_dec.pdf')
+
+
 
 
 # Current location
@@ -175,7 +165,15 @@ plt.savefig('../figures/smc_population_ra_dec.pdf')
 # plt_kwargs = {'colors':'k'}
 # density_contour.density_contour(HMXB[0], HMXB[1], nbins_x=40, nbins_y=40, **plt_kwargs)
 # plt.tight_layout()
-plt.figure(figsize=(8,8))
-sf_history.get_SMC_plot_polar(40.0, ra_dist=HMXB[0], dec_dist=HMXB[1], xwidth=3.0, ywidth=3.0)
+fig, ax = plt.subplots(2, 1, figsize=(4.5,7))
+#plt.figure(figsize=(5,5))
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+ax[1].set_xticks([])
+ax[1].set_yticks([])
+sf_history.get_SMC_plot_polar(12.0, ra_dist=HMXB[0], dec_dist=HMXB[1], fig_in=fig, rect=211, xwidth=3.0, ywidth=3.0)
+sf_history.get_SMC_plot_polar(40.0, ra_dist=HMXB[0], dec_dist=HMXB[1], fig_in=fig, rect=212, xwidth=3.0, ywidth=3.0)
 plt.tight_layout()
 plt.savefig('../figures/smc_population_HMXB_ra_dec.pdf')
+
+
