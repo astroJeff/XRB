dae2516662d1a25d10cbaf915215b767af7c0f3e
astroJeff <andrews@astro.columbia.edu>
2016-09-29 15:32:44 +0300

    Added setup.py to repository

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='xrb_mcmc',
+      version='0.1',
+      description='High Mass X-Ray Binary Evolution with MCMC',
+      url='http://github.com/astroJeff/XRB',
+      author='Jeff Andrews',
+      author_email='andrews@physics.uoc.gr',
+      license='',
+      packages=find_packages(),
+      zip_safe=False)
